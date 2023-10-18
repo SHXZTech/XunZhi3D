221b8a8bc5ac4386b903debe0622b9ad9e1343e8
Thomas H <emailhutao@163.com>
2023-10-18 18:59:53 +0800

    backup before rtk

---
+++ LidarScannerDemo/Model/GeoSensorModel.swift
@@ -0,0 +1,8 @@
+//
+//  GeoSensorModel.swift
+//  LidarScannerDemo
+//
+//  Created by Tao Hu on 2023/10/18.
+//
+
+import Foundation
---
+++ LidarScannerDemo/Model/RtkModel.swift
@@ -0,0 +1,11 @@
+//
+//  RtkModel.swift
+//  LidarScannerDemo
+//
+//  Created by Tao Hu on 2023/10/18.
+//
+
+import Foundation
+//import LiteRTK
+
+
