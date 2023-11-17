ffcd9b88d7651b333b21771cb5f119b6f81b3cc6
Thomas H <emailhutao@163.com>
2023-11-17 13:31:54 +0800

    solve captureView not load bug

---
+++ LidarScannerDemo/Views/ModelInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelInfoView.swift
+//  LidarScannerDemo
+//
+//  Created by Tao Hu on 2023/11/17.
+//
+
+import SwiftUI
+
+struct ModelInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelInfoView()
+}
---
+++ LidarScannerDemo/Views/VersionView.swift
@@ -0,0 +1,18 @@
+//
+//  VersionView.swift
+//  LidarScannerDemo
+//
+//  Created by Tao Hu on 2023/11/17.
+//
+
+import SwiftUI
+
+struct VersionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VersionView()
+}
